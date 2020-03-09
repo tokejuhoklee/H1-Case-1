b7f755e3c8e9b488ced0b9c867a4aaf3356abdb6
Toke Nielsen <57397227+tokejuhoklee@users.noreply.github.com>
2020-03-09 13:05:43 +0100

    .

--- Program.cs
+++ Program.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#region opgavebeskrivelse
+//1. Man skal kunne oprette, rette og slette en sommerhusejere
+//2. Man skal kunne oprette, rette og slette en sommerhusejere
+//3. Man skal kunne oprette, rette og slette en sommerhusejere
+//4.Man skal kunne få listet et sommerhus reservationer, samt få vist en specifikation af den eneklte udlejning
+//5. En ejer kan godt have glere sommerhuse
+//6.Man skal kunnne administrere områder
+//7.Man skal kunne administrere "sæsonkategori og tilhørende priser
+//8.Man skal kunne administrere Inspektiører
+#endregion
+
 
 namespace H1_Case1
 {
