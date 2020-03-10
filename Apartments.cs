96817221860d2bd15848ac253b7ab68ed1387a48
Toke Nielsen <57397227+tokejuhoklee@users.noreply.github.com>
2020-03-10 14:38:23 +0100

    Added the bare classes for the different objects so they can be created as objects later

---
+++ Apartments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Case1
+{
+    public class Apartments
+    {
+        public int ApartmentId { get; set; }
+        public int ApartmentSuperindendantContractId { get; set; }
+        public int IndependantOverseerId { get; set; }
+        public int Area { get; set; }
+        public int AddressId { get; set; }
+        public int Size { get; set; }
+        public int Rooms { get; set; }
+        public int NumberOfBeds { get; set; }
+        public string RentalQuality { get; set; }
+        public string Etc { get; set; }
+    }
+}
---
+++ CustomerRentalContractRelation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Case1
+{
+    public class CustomerRentalContractRelation
+    {
+        public int CustomerConsultantId { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerRentalContractText { get; set; }
+        public DateTime DateOfContract { get; set; }
+        public DateTime DateStartRent { get; set; }
+        public DateTime DateEndRent { get; set; }
+        public double Price { get; set; }
+    }
+}
