6d5d351c9fc6e90b16a0439505a7fc59ddcc8582
Toke Nielsen <57397227+tokejuhoklee@users.noreply.github.com>
2020-03-11 12:33:49 +0100

    Improved test for database connection, and minor edits to the ER-Diagram

--- DBConnectionTest.cs
+++ DBConnectionTest.cs
@@ -31,6 +31,7 @@ namespace H1_Case1
             }
             connectionStringUse(username, password);
 
+            
         }
 
         public static void connectionStringUse(string userName,string userPass)
@@ -39,20 +40,63 @@ namespace H1_Case1
             SqlConnection connect;
             connectionString = "Data Source = W2K19SQL.hq.gollomotors.dk; Initial Catalog = Sydvest-Bo; User ID = "+userName +"; Password ="+userPass;
             connect = new SqlConnection(connectionString);
+            string query1;
+            Console.WriteLine("\nIndtast Sql Statement: ");
+            query1 = Console.ReadLine();
+            string query2 = "Select* from [Address]";
+            
+
             try
             {
+                SqlCommand command1;
+                SqlCommand command2;
+                SqlDataReader sqlReader =null;
+                    
+
+
                 connect.Open();
                 Console.WriteLine("Connection Open ! ");
+                command2 = new SqlCommand(query2, connect);
+                command1 = new SqlCommand(query1, connect);
+
+                sqlReader = command2.ExecuteReader();
+
+
+                if (sqlReader.HasRows)
+                {
+                    while (sqlReader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", sqlReader.GetInt32(0),
+                            sqlReader.GetString(1));
+                        Console.WriteLine(sqlReader.GetValue(0)+" - "+sqlReader.GetValue(1) + " - " + sqlReader.GetValue(0));
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                        sqlReader.Close();
+
+
+    
+
+               
                 connect.Close();
+                Console.Read();
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex);
                 Console.WriteLine("Can not open connection ! ");
+                Console.ReadKey();
+                Console.Clear();
+                DBConnectionTest newConnect = new DBConnectionTest();
+
             }
             connect.Close();
 
-            Console.Read();
-        }
+            }
 
         
 
