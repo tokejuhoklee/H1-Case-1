dbc40eaa83aa85899cc9aacc86cd91e7f8714ef2
Toke Nielsen <57397227+tokejuhoklee@users.noreply.github.com>
2020-03-13 15:47:59 +0100

    Created sql statement to bulk insert to zip codes

--- Data/zipCode.sql
+++ Data/zipCode.sql
@@ -2,13 +2,12 @@ use [Sydvest-Bo2]
 go
 
 BULK INSERT [Zip Code and Town]
-FROM '/Data/PostalCodes.csv'
+FROM '/Users/dreams/Programming/H1-Case-1/Data/PostalCodes.csv'
 WITH (FIRSTROW = 1,
     FIELDTERMINATOR = ',',
     ROWTERMINATOR='\n',
     BATCHSIZE=250000,
     MAXERRORS=2,
 	codepage=65001);
-
 
 
