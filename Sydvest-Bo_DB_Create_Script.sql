f35ff68242441e65566ae63bcf8402f1ca8ea448
GolloMotorS <rhoerlyk@gmail.com>
2020-03-16 13:13:30 +0100

    Opdateret ER-Diagram og SQL script, fjernet de gamle udkast.: Sydvest-Bo_DB.... .sql og .vuerd.json

--- Sydvest-Bo_DB_Create_Script.sql
+++ Sydvest-Bo_DB_Create_Script.sql
@@ -14,17 +14,17 @@ go
 
 CREATE TABLE "Address"
 (
-  "Address id" int           NOT NULL IDENTITY(1,1),
-  "ZipCode id" int           NOT NULL,
-  "Adresse"    nvarchar(127) NOT NULL DEFAULT '',
+  "Address id" int                                                      NOT NULL IDENTITY(1,1),
+  "ZipCode id" int  check ("ZipCode id" > 999 and "ZipCode id" < 10000) NOT NULL,
+  "Address"    nvarchar(127)                                            NOT NULL DEFAULT '',
   CONSTRAINT "PK_Address" PRIMARY KEY ("Address id")
 )
 GO
 
-CREATE TABLE Customer
+CREATE TABLE "Customer"
 (
   "Customer id" int      NOT NULL IDENTITY(1,1),
-  "Person id"   int      NOT NULL,
+  "Person id"   int      NOT NULL IDENTITY(1,1),
   "Notes"       nvarchar NOT NULL DEFAULT '',
   CONSTRAINT "PK_Customer" PRIMARY KEY ("Customer id")
 )
@@ -53,7 +53,7 @@ CREATE TABLE "Customer Rental Contract"
 )
 GO
 
-CREATE TABLE District
+CREATE TABLE "District"
 (
   "Area id"              int           NOT NULL IDENTITY(1,1),
   "Area name"            nvarchar(200) NOT NULL,
@@ -62,23 +62,24 @@ CREATE TABLE District
 )
 GO
 
-CREATE TABLE "Independant overseer"
+CREATE TABLE "Independant Overseer"
 (
   "Independant overseer id" int      NOT NULL IDENTITY(1,1),
   "Person id"               int      NOT NULL,
   "Etc"                     nvarchar NOT NULL DEFAULT '',
-  CONSTRAINT "PK_Independant overseer" PRIMARY KEY ("Independant overseer id")
+  CONSTRAINT "PK_Independant Overseer" PRIMARY KEY ("Independant overseer id")
 )
 GO
 
-CREATE TABLE Person
+CREATE TABLE "Person"
 (
   "Person id"  int           NOT NULL IDENTITY(1,1),
   "Address id" int           NOT NULL,
   "Name"       nvarchar(47)  NOT NULL DEFAULT '',
-  "Last name"  nvarchar(79)  NOT NULL DEFAULT '',
+  "Last Name"  nvarchar(79)  NOT NULL DEFAULT '',
   "Email"      nvarchar(127) NOT NULL DEFAULT '',
   "PhoneNo"    nvarchar(20)  NOT NULL DEFAULT '',
+  "Password"   nvarchar(31)  NOT NULL DEFAULT 'Spassw0rd',
   CONSTRAINT "PK_Person" PRIMARY KEY ("Person id")
 )
 GO
@@ -91,7 +92,7 @@ CREATE TABLE "Rental Consultant"
 )
 GO
 
-CREATE TABLE Residence
+CREATE TABLE "Residence"
 (
   "Residence id"                int          NOT NULL IDENTITY(1,1),
   "Residence Owner Contract id" int          NOT NULL,
@@ -104,7 +105,7 @@ CREATE TABLE Residence
   "Rental quality"              nvarchar(47) NOT NULL DEFAULT '',
   "Etc"                         nvarchar     NOT NULL DEFAULT '',
   "Base Price"                  money        NOT NULL DEFAULT 0.0,
-  "Residence Type"              nvarchar(31) NOT NULL DEFAULT '',
+  "Residence Type"              nvarchar(31) NOT NULL DEFAULT 'Sommerhouse',
   CONSTRAINT "PK_Residence" PRIMARY KEY ("Residence id")
 )
 GO
@@ -132,7 +133,7 @@ CREATE TABLE "Residence Owner Contract"
 )
 GO
 
-CREATE TABLE Vacancy
+CREATE TABLE "Vacancy"
 (
   "year"                        date  NOT NULL,
   "week"                        date  NOT NULL,
@@ -145,8 +146,8 @@ GO
 
 CREATE TABLE "ZipCode Town"
 (
-  "ZipCode id" int  check ("ZipCode id" > 99 and "ZipCode id" < 10000) NOT NULL,
-  "Town"       nvarchar(79)                                            NOT NULL DEFAULT '''',
+  "ZipCode id" int  check ("ZipCode id" > 999 and "ZipCode id" < 10000) NOT NULL,
+  "Town"       nvarchar(79)                                             NOT NULL DEFAULT '''',
   CONSTRAINT "PK_ZipCode Town" PRIMARY KEY ("ZipCode id")
 )
 GO
@@ -155,31 +156,31 @@ ALTER TABLE "ZipCode Town"
   ADD CONSTRAINT "UQ_ZipCode id" UNIQUE ("ZipCode id")
 GO
 
-ALTER TABLE Residence
+ALTER TABLE "Residence"
   ADD CONSTRAINT "FK_District_TO_Residence"
-    FOREIGN KEY (Area)
-    REFERENCES District ("Area id")
+    FOREIGN KEY ("Area")
+    REFERENCES "District" ("Area id")
 GO
 
 ALTER TABLE "Residence Owner"
   ADD CONSTRAINT "FK_Person_TO_Residence Owner"
     FOREIGN KEY ("Person id")
-    REFERENCES Person ("Person id")
+    REFERENCES "Person" ("Person id")
 GO
 
 ALTER TABLE "Customer Consultant"
   ADD CONSTRAINT "FK_Person_TO_Customer Consultant"
     FOREIGN KEY ("Person id")
-    REFERENCES Person ("Person id")
+    REFERENCES "Person" ("Person id")
 GO
 
 ALTER TABLE "Rental Consultant"
   ADD CONSTRAINT "FK_Person_TO_Rental Consultant"
     FOREIGN KEY ("Person id")
-    REFERENCES Person ("Person id")
+    REFERENCES "Person" ("Person id")
 GO
 
-ALTER TABLE District
+ALTER TABLE "District"
   ADD CONSTRAINT "FK_Rental Consultant_TO_District"
     FOREIGN KEY ("Rental consultant id")
     REFERENCES "Rental Consultant" ("Rental Consultant id")
@@ -188,22 +189,22 @@ GO
 ALTER TABLE "Customer Rental Contract"
   ADD CONSTRAINT "FK_Customer_TO_Customer Rental Contract"
     FOREIGN KEY ("Customer id")
-    REFERENCES Customer ("Customer id")
+    REFERENCES "Customer" ("Customer id")
 GO
 
-ALTER TABLE Customer
+ALTER TABLE "Customer"
   ADD CONSTRAINT "FK_Person_TO_Customer"
     FOREIGN KEY ("Person id")
-    REFERENCES Person ("Person id")
+    REFERENCES "Person" ("Person id")
 GO
 
-ALTER TABLE "Independant overseer"
-  ADD CONSTRAINT "FK_Person_TO_Independant overseer"
+ALTER TABLE "Independant Overseer"
+  ADD CONSTRAINT "FK_Person_TO_Independant Overseer"
     FOREIGN KEY ("Person id")
-    REFERENCES Person ("Person id")
+    REFERENCES "Person" ("Person id")
 GO
 
-ALTER TABLE Person
+ALTER TABLE "Person"
   ADD CONSTRAINT "FK_Address_TO_Person"
     FOREIGN KEY ("Address id")
     REFERENCES "Address" ("Address id")
@@ -215,22 +216,22 @@ ALTER TABLE "Address"
     REFERENCES "ZipCode Town" ("ZipCode id")
 GO
 
-ALTER TABLE Residence
+ALTER TABLE "Residence"
   ADD CONSTRAINT "FK_Residence Owner Contract_TO_Residence"
     FOREIGN KEY ("Residence Owner Contract id")
     REFERENCES "Residence Owner Contract" ("Residence Owner Contract id")
 GO
 
-ALTER TABLE Residence
+ALTER TABLE "Residence"
   ADD CONSTRAINT "FK_Address_TO_Residence"
     FOREIGN KEY ("Address id")
-    REFERENCES Address ("Address id")
+    REFERENCES "Address" ("Address id")
 GO
 
-ALTER TABLE Residence
-  ADD CONSTRAINT "FK_Independant overseer_TO_Residence"
+ALTER TABLE "Residence"
+  ADD CONSTRAINT "FK_Independant Overseer_TO_Residence"
     FOREIGN KEY ("Independant overseer id")
-    REFERENCES "Independant overseer" ("Independant overseer id")
+    REFERENCES "Independant Overseer" ("Independant overseer id")
 GO
 
 ALTER TABLE "Residence Owner Contract"
@@ -239,13 +240,13 @@ ALTER TABLE "Residence Owner Contract"
     REFERENCES "Rental Consultant" ("Rental Consultant id")
 GO
 
-ALTER TABLE Vacancy
+ALTER TABLE "Vacancy"
   ADD CONSTRAINT "FK_Residence_TO_Vacancy"
     FOREIGN KEY ("Residence id")
-    REFERENCES Residence ("Residence id")
+    REFERENCES "Residence" ("Residence id")
 GO
 
-ALTER TABLE Vacancy
+ALTER TABLE "Vacancy"
   ADD CONSTRAINT "FK_Residence Owner Contract_TO_Vacancy"
     FOREIGN KEY ("Residence Owner Contract id")
     REFERENCES "Residence Owner Contract" ("Residence Owner Contract id")
@@ -268,47 +269,3 @@ ALTER TABLE "Residence Owner Contract"
     FOREIGN KEY ("Residence Owner id")
     REFERENCES "Residence Owner" ("Residence Owner id")
 GO
-
------- USER controle
-USE "Sydvest-Bo"
-GO
-
-ALTER ROLE db_datareader ADD MEMBER tec
-GO
-
-ALTER ROLE db_datawriter ADD MEMBER tec
-GO
-
-ALTER ROLE db_ddladmin ADD MEMBER tec
-go
-
----- Insert from Resources
-/*
-BULK INSERT "ZipCode Town"
-FROM 'd:\TEC.Datatekniker\H1\Versionering og Dokumentation\H1-Case-1\Resources\postnumre.csv'
-WITH
-(
-    FORMAT = 'CSV', 
-    FIELDQUOTE = '"',
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ';',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
-)
-GO
-*/
-
-/*
--- BULK INSERT "ZipCode Town"
-BULK INSERT [dbo].[ZipCode Town]
-FROM 'd:\TEC.Datatekniker\H1\Versionering og Dokumentation\H1-Case-1\Resources\postnumre.csv'
-with (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    BATCHSIZE = 250000,
-    MAXERRORS = 2,
-    CODEPAGE = 65001
-);
-GO
-*/
