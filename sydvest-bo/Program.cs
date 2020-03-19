ad6754f077c77fb7493478f6ac3bc26f94e5bc7f
Toke Nielsen <57397227+tokejuhoklee@users.noreply.github.com>
2020-03-19 21:42:01 +0100

    .

--- sydvest-bo/Program.cs
+++ sydvest-bo/Program.cs
@@ -29,47 +29,59 @@ namespace sydvest_bo
 
         static void Main(string[] args)
         {
-            // init windows size
-            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            
+
+
+
+            TestProgram.Test();
+
+            Console.WriteLine("tryk på en tast for at åbne menu");
+            Console.ReadKey();
+            menuflytTemp();
+
+        }//END static void Main(string[] args)
+        public static void menuflytTemp() { 
+        // init windows size
+        Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             ShowWindow(ThisConsole, MAXIMIZE);
-            // Figure out other common windows sizes
-            int currentConsoleWindowWidth = Console.WindowWidth;
-            int currentConsoleWindowHeight = Console.WindowHeight;
+        // Figure out other common windows sizes
+        int currentConsoleWindowWidth = Console.WindowWidth;
+        int currentConsoleWindowHeight = Console.WindowHeight;
 
-            int standardWindowX = 25;
-            int standardWindowY = 25;
-            int standardWindowWidth = currentConsoleWindowWidth - (standardWindowX + 3);
-            int standardWindowHeight = currentConsoleWindowHeight - (standardWindowY + 3);
+        int standardWindowX = 25;
+        int standardWindowY = 25;
+        int standardWindowWidth = currentConsoleWindowWidth - (standardWindowX + 3);
+        int standardWindowHeight = currentConsoleWindowHeight - (standardWindowY + 3);
 
-            // Colors shor abravation
-            ConsoleColor blaaM = ConsoleColor.DarkBlue;
-            ConsoleColor blaa = ConsoleColor.Blue;
-            ConsoleColor cyanM = ConsoleColor.DarkCyan;
-            ConsoleColor cyan = ConsoleColor.Cyan;
-            ConsoleColor groenM = ConsoleColor.DarkGreen;
-            ConsoleColor groen = ConsoleColor.Green;
-            ConsoleColor gulM = ConsoleColor.DarkYellow;
-            ConsoleColor gul = ConsoleColor.Yellow;
-            ConsoleColor roedM= ConsoleColor.DarkRed;
-            ConsoleColor roed = ConsoleColor.Red;
-            ConsoleColor magentaM = ConsoleColor.DarkMagenta;
-            ConsoleColor Magenta = ConsoleColor.Magenta;
-            ConsoleColor sort = ConsoleColor.Black;
-            ConsoleColor graaM = ConsoleColor.DarkGray;
-            ConsoleColor graa = ConsoleColor.Gray;
-            ConsoleColor hvid = ConsoleColor.White;
+        // Colors shor abravation
+        ConsoleColor blaaM = ConsoleColor.DarkBlue;
+        ConsoleColor blaa = ConsoleColor.Blue;
+        ConsoleColor cyanM = ConsoleColor.DarkCyan;
+        ConsoleColor cyan = ConsoleColor.Cyan;
+        ConsoleColor groenM = ConsoleColor.DarkGreen;
+        ConsoleColor groen = ConsoleColor.Green;
+        ConsoleColor gulM = ConsoleColor.DarkYellow;
+        ConsoleColor gul = ConsoleColor.Yellow;
+        ConsoleColor roedM = ConsoleColor.DarkRed;
+        ConsoleColor roed = ConsoleColor.Red;
+        ConsoleColor magentaM = ConsoleColor.DarkMagenta;
+        ConsoleColor Magenta = ConsoleColor.Magenta;
+        ConsoleColor sort = ConsoleColor.Black;
+        ConsoleColor graaM = ConsoleColor.DarkGray;
+        ConsoleColor graa = ConsoleColor.Gray;
+        ConsoleColor hvid = ConsoleColor.White;
 
-            // testText
-            string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tellus velit, aliquam quis eros vel, malesuada bibendum mi. Mauris non ultricies mauris, at accumsan tellus. Etiam tincidunt venenatis nisl in molestie.Nunc quis.";
+        // testText
+        string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tellus velit, aliquam quis eros vel, malesuada bibendum mi. Mauris non ultricies mauris, at accumsan tellus. Etiam tincidunt venenatis nisl in molestie.Nunc quis.";
 
-            string loginWindowText1Welcome = String.Format($"Velkommen til Sydvest-Bo\n\nDu skal logge ind for at kunne bruge programmet.\n\nEt blankt password vil afslutte programmet.");
-            string loginWindowText2user = String.Format($"Brugernavn :");
-            string loginWindowText3password = String.Format($"Password   :");
+        string loginWindowText1Welcome = String.Format($"Velkommen til Sydvest-Bo\n\nDu skal logge ind for at kunne bruge programmet.\n\nEt blankt password vil afslutte programmet.");
+        string loginWindowText2user = String.Format($"Brugernavn :");
+        string loginWindowText3password = String.Format($"Password   :");
 
-            Window LoginWindow = new Window(standardWindowX, standardWindowY, standardWindowWidth, standardWindowHeight, 0, hvid, graaM, sort, false, "Login");
-            Frame LoginWindow1user = new Frame(standardWindowX, standardWindowY+8, standardWindowWidth, 6, 0, sort, graa, sort, false);
-            Frame LoginWindow2password = new Frame(standardWindowX, standardWindowY+10, standardWindowWidth, 6, 0, sort, graa, sort, false);
-            LoginWindow.Visible = true;
+        Window LoginWindow = new Window(standardWindowX, standardWindowY, standardWindowWidth, standardWindowHeight, 0, hvid, graaM, sort, false, "Login");
+        Frame LoginWindow1user = new Frame(standardWindowX, standardWindowY + 8, standardWindowWidth, 6, 0, sort, graa, sort, false);
+        Frame LoginWindow2password = new Frame(standardWindowX, standardWindowY + 10, standardWindowWidth, 6, 0, sort, graa, sort, false);
+        LoginWindow.Visible = true;
             LoginWindow1user.Visible = true;
             LoginWindow2password.Visible = true;
             LoginWindow.Draw();
@@ -77,8 +89,8 @@ namespace sydvest_bo
 
             // LoginWindow1user.Print(loginWindowText2user);
             bool userValidated = false;
-            string username = LoginWindow1user.ReadInput(loginWindowText2user);
-            string password = LoginWindow2password.ReadInput(loginWindowText3password);
+        string username = LoginWindow1user.ReadInput(loginWindowText2user);
+        string password = LoginWindow2password.ReadInput(loginWindowText3password);
 
             // validat user: Get user type
             if (username != "" || password != "")
@@ -89,29 +101,24 @@ namespace sydvest_bo
             while (userValidated)
             {
                 Window MainHead = new Window(1, 1, currentConsoleWindowWidth - 3, 4, 0, hvid, blaaM, sort, true, "Main Head");
-                MainHead.Draw();
+    MainHead.Draw();
                 MainHead.Print("");
 
                 Window MainMenu = new Window(1, 6, 25, currentConsoleWindowHeight - 8, 0, hvid, blaaM, sort, true, "Menu");
-                MainMenu.Draw();
+    MainMenu.Draw();
                 MainMenu.Print("");
 
                 Window MainWindow = new Window(standardWindowX + 27, standardWindowY + 4, standardWindowWidth - (27 + 4), standardWindowHeight - (4 + 4), 0, hvid, blaaM, sort, true, "Hoved Vindue");
-                string mainWindowText1 = String.Format($"Du er nu logget ind {username} og kan bruge programmet\n\nPisse fedt for det afsluttes nu, hvis du trykker på en tast.");
-                MainWindow.Draw();
+    string mainWindowText1 = String.Format($"Du er nu logget ind {username} og kan bruge programmet\n\nPisse fedt for det afsluttes nu, hvis du trykker på en tast.");
+    MainWindow.Draw();
                 MainWindow.Print(mainWindowText1);
 
                 Console.ReadKey(true);
 
             }// End while (userValidated)  *********** user loged out
-
-
-
-            // exit program propperly : cloes files ect.
+             // exit program propperly : cloes files ect.
 
             // calling testProgram calss
-            TestProgram.Test();
-
-        }//END static void Main(string[] args)
+        }
     }
 }
