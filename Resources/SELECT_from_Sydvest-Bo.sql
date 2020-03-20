cd31d9a1b5559faa2655621d5f66871a4078aabf
GolloMotorS <rhoerlyk@gmail.com>
2020-03-20 16:20:28 +0100

    Alle SQL Resources er opdateret og virker, tilføjet Bruger kontrol i Sydvest-Bo_DB_Create_Script.sql og lavet Sydvest-Bo_test_00_Insert_All_Test_Data.sql, resten har fået numre til afviklings orden

--- Resources/SELECT_from_Sydvest-Bo.sql
+++ Resources/SELECT_from_Sydvest-Bo.sql
@@ -1,15 +1,20 @@
 USE "Sydvest-Bo"
 GO
 
-SELECT * FROM "Adresse"
+SELECT * FROM Adresse
+-- where Adressestring = 'Sydvest-Bo admin 1 1. tv.'
 GO
 
-SELECT * FROM "Distrikt"
+SELECT * FROM Distrikt
 GO
 
-SELECT * FROM "Ejer"
+SELECT * FROM Ejer
+  JOIN Person ON Ejer.Personid = Person.Personid
+  JOIN Adresse ON Person.Adresseid = Adresse.Adresseid
+  JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
 GO
 
+print 'Feriebolig';
 SELECT * FROM "Feriebolig"
   JOIN Distrikt ON Feriebolig.Distriktid = Distrikt.Distriktid
   JOIN Ejer ON Feriebolig.Ejerid = Ejer.Ejerid
@@ -17,17 +22,38 @@ SELECT * FROM "Feriebolig"
   JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
 GO
 
-SELECT * FROM "Kunde"
+print 'Feriebolig - Sommerhuse';
+SELECT * FROM "Feriebolig"
+  JOIN Distrikt ON Feriebolig.Distriktid = Distrikt.Distriktid
+  JOIN Ejer ON Feriebolig.Ejerid = Ejer.Ejerid
+  JOIN Adresse ON Feriebolig.Adresseid = Adresse.Adresseid
+  JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
+  WHERE FeriboligType = 'Sommerhus'
+GO
+
+print 'Feriebolig - Ferielejlighed';
+SELECT * FROM "Feriebolig"
+  JOIN Distrikt ON Feriebolig.Distriktid = Distrikt.Distriktid
+  JOIN Ejer ON Feriebolig.Ejerid = Ejer.Ejerid
+  JOIN Adresse ON Feriebolig.Adresseid = Adresse.Adresseid
+  JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
+  WHERE FeriboligType = 'Ferielejlighed'
+GO
+
+
+SELECT * FROM Kunde
+  JOIN Person ON Kunde.Personid = Person.Personid
+  JOIN Adresse ON Person.Adresseid = Adresse.Adresseid
+  JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
 GO
 
 SELECT * FROM Kundekonsulent
   JOIN Person ON Kundekonsulent.Personid = Person.Personid
   JOIN Adresse ON Person.Adresseid = Adresse.Adresseid
   JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
-
 GO
 
-SELECT * FROM "Lejekontrakt"
+SELECT * FROM Lejekontrakt
 GO
 
 SELECT * FROM Opsynsmand
@@ -36,12 +62,12 @@ SELECT * FROM Opsynsmand
   JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
 GO
 
-SELECT * FROM "Person"
+SELECT * FROM Person
   JOIN Adresse ON Person.Adresseid = Adresse.Adresseid
   JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
 GO
 
-SELECT * FROM "Saesonkategori"
+SELECT * FROM Saesonkategori
 GO
 
 SELECT * FROM Udlejningskonsulent
@@ -50,6 +76,6 @@ SELECT * FROM Udlejningskonsulent
   JOIN PostnrBy ON Adresse.Postnr = PostnrBy.Postnr
 GO
 
-SELECT * FROM "Udlejningskontrakt"
+SELECT * FROM Udlejningskontrakt
 GO
 
--- Sydvest-Bo_DB_Create_Script.sql
+++ Sydvest-Bo_DB_Create_Script.sql
@@ -278,17 +278,41 @@ ALTER TABLE Adresse
     REFERENCES PostnrBy (Postnr)
 GO
 
-/*
------- USER controle
-GRANT SELECT, INSERT, UPDATE, DELETE ON "Sydvest-Bo" TO tec;
-USE "Sydvest-Bo"
+-- Creates the login tec with password 'OsteFis' ------------------------------------------------------
+
+USE master
+IF NOT EXISTS (SELECT name FROM sys.sql_logins WHERE name='tec')
+  BEGIN
+    CREATE LOGIN tec
+      WITH PASSWORD = 'OsteFis';
+    PRINT 'USER ''tec'' CREATED';
+  END
+ELSE
+  BEGIN
+    PRINT 'USER ''tec'' ALREADY EXISTS';
+  END;
+
+
+-- map user rights
+
+USE [Sydvest-Bo]
+GO
+-- Check database user
+IF USER_ID('tec') IS NULL
+  CREATE USER tec FOR LOGIN tec;
 GO
 
-EXEC sp_addrolemember 'db_datareader', 'tec'
+USE [Sydvest-Bo]
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [tec]
+GO
+USE [Sydvest-Bo]
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [tec]
+GO
+USE [Sydvest-Bo]
+GO
+ALTER ROLE [db_ddladmin] ADD MEMBER [tec]
+GO
+-------------------------------------------------------
 
-EXEC sp_addrolemember 'db_datawriter', 'tec'
-
-EXEC sp_addrolemember 'db_ddladmin', 'tec'
-
-go
-*/
